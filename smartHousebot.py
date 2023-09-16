4f4fe701cb1835a767864c307d6303d02b7ce552
kagg0n <93994793+kagg0n@users.noreply.github.com>
2023-09-16 16:33:54 +0300

    telebot ver 2
    добавлена функция обмена данными с rpi pico

--- smartHousebot.py
+++ smartHousebot.py
@@ -1,21 +1,20 @@
+#---------LIBARARIES---------------
+
 import telebot
 from telebot import types
+import socket
 
-#DEVICE TYPES
-# 1 - LED
-# 2 - TEMPERATURE MEASURING
-# 3 - LIGHT MEASURING
-# 4 PRESSURE MEASURING
+#--------SOCKET SETTINGS------------
 bot = telebot.TeleBot('6570224062:AAG43G3pLc3DnB3BFFbQC9_jCvKpywZ3BAs')
 
 #DEVICE PARAMETRS (back part of the project that will use micropython to communicate with sensors, lamps and etc)
 
 #---LED---
-led_status = 0 # on/off
+led_value = '' # on/off
 
 #---TEMPERATURE MEASURING---
-tempMeasure  = 27
-humidityMeasure = 60
+tempValues = ''
+humidityValues = ''
 
 #---LIGHT MEASURING----
 lightMeasure = 100
@@ -24,25 +23,30 @@ lightMeasure = 100
 
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    led_button = types.KeyboardButton(text='LED лента')
+    led_button = types.KeyboardButton( text="LED лента")
     tempMeasure = types.KeyboardButton(text = 'Температура')
     lightMeasure = types.KeyboardButton(text = 'Освещенность')
+    updateData = types.KeyboardButton(text = "Обновить данные")
+
 
     markup.row(led_button,tempMeasure)
     markup.add(lightMeasure)
+    markup.add(updateData)
     bot.send_message(message.from_user.id, "👋Приветствую, я чат-бот, созданный для управления умным домом")
     bot.send_message(message.from_user.id, "Все доступные на данный момент функции будут отображены внизу⬇️")
+    bot.send_message(message.from_user.id, "Основное меню чат-бота:")
     bot.send_message(message.from_user.id, "Умные устройства:\n 🔸Управляемая LED лента \n 🔸Датчик температуры \n 🔸Датчик влажности \n 🔸Датчик освещенности",reply_markup = markup)
-    bot.send_message(message.from_user.id, "Основное меню чат-бота:",reply_markup = markup)
 
 def main_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    led_button1 = types.InlineKeyboardButton(text='LED лента')
+    led_button1 = types.KeyboardButton(text='LED лента')
     tempMeasure1 = types.KeyboardButton(text = 'Температура')
     lightMeasure1 = types.KeyboardButton(text = 'Освещенность')
+    updateData1 = types.KeyboardButton(text = "Обновить данные")
 
     markup.row(led_button1,tempMeasure1)
     markup.add(lightMeasure1)
+    markup.add(updateData1)
     bot.send_message(message.from_user.id, "Основное меню чат-бота:")
     bot.send_message(message.from_user.id, "Умные устройства:\n 🔸Управляемая LED лента \n 🔸Датчик температуры \n 🔸Датчик влажности \n 🔸Датчик освещенности",reply_markup = markup)
 @bot.message_handler(content_types=['text'])
@@ -50,9 +54,32 @@ def main_menu(message):
 
 def get_text_message(message):
     #-----------------LED SETTINGS-----------------------
+
     global status
+    global rawData
+    global tempValues
+    global led_value
+    if message.text == "Обновить данные":
+        sock = 0
+        try:
+            led_value = ''
+            tempValues = ''
+            humidityValues = ''
+            sock = socket.socket()
+            sock.connect(('192.168.0.104', 3030))
+            key = sock.send('1'.encode('utf-8'))
+            data = sock.recv(1024) # Получаем данные из сокета.
+            rawData = data.decode('utf-8')
+            sock.close()
+            decoding_protocol()
+            bot.send_message(message.from_user.id, "Обновили данные умного дома")
+            main_menu(message)
+        except:
+            bot.send_message(message.from_user.id, "Не удалось подключиться к хабу умного дома")
+            sock.close()
+            main_menu(message)
     if message.text == "LED лента":
-        if led_status == 1:
+        if led_value == '1':
             status = "Устройство включено🔆"
         else:
             status = "Устройство отключено💤"
@@ -65,22 +92,37 @@ def get_text_message(message):
         markup.add(backMenu1)
         bot.send_message(message.from_user.id, "Тип устройства: LED лента")
         bot.send_message(message.from_user.id, status, reply_markup = markup)
+
     #--------------TEMPERATURE-----------------------------
     # need to add temp measuring in real time
+
     if message.text == "Температура":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        realTemp = types.KeyboardButton('Измерить температуру')
         backMenu = types.InlineKeyboardButton('Вернуться в главное меню💾')
-        markup.add(realTemp)
         markup.add(backMenu)
         bot.send_message(message.from_user.id, "Тип устройства: датчик температуры📈")
         bot.send_message(message.from_user.id, "Температура на данный момент:")
-        bot.send_message(message.from_user.id, tempMeasure, reply_markup = markup)
-        bot.send_message(message.from_user.id, "Подсказка: Вы также можете измерить температуру в помещении в реальном времени(пока недоступно)")
+        bot.send_message(message.from_user.id, tempValues, reply_markup = markup)
         
     #-------------MAIN MENU--------------------------------
+
     if message.text == "Вернуться в главное меню💾":
         main_menu(message)
+
+def decoding_protocol():
+	global led_value
+	global tempValues
+	global humidityValues
     
-        
+	i = 0
+	while rawData[i] != '/':
+		tempValues = tempValues + rawData[i]
+		i = i + 1
+	i = i + 1
+	while rawData[i] != '/':
+		humidityValues = humidityValues + rawData[i]
+		i = i + 1
+	for n in range(len(rawData) - (i + 1)):
+		f = n + 1
+		led_value = led_value + rawData[i + f]
 bot.polling(none_stop=True, interval=0)
